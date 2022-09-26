3d70d7bde5fbf6178f8cc9dc0758a235a3985a1a
Harshit Chaudhary <90213465+dnharshit@users.noreply.github.com>
2022-09-26 18:16:17 +0530

    Time: 16 ms (5.22%), Space: 6.5 MB (5.20%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -10,7 +10,7 @@ public:
         else{
             ::reverse(s.begin(), s.end());
         }
-        cout << s;
+        // cout << s;
         long long int y;
         try{
             y = stoi(s);    
@@ -18,14 +18,6 @@ public:
         catch(...){
             return 0;
         }
-        // long long int z = (long long int) y;
-        // if(y>2147483648){
-        //     return 0;
-        // }
-        // if(wasNegative==1){
-        //     cout << "*";
-        //     return z*-1;
-        // }
         
         return y;
         
